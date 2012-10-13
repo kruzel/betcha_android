7ac554cbdfd0cc2f9c6d80186d37954a60d456c1
ofer kruzel <okruzel@gmail.com>
2012-10-13 22:37:22 +0200

    fix server ip

--- src/com/betcha/adapter/PredictionWithCbxAdapter.java
+++ src/com/betcha/adapter/PredictionWithCbxAdapter.java
@@ -9,7 +9,6 @@ import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
